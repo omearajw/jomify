8880497aa752515a73dc4774180d310593b096b3
TallMonkee <omeara.jw@gmail.com>
2026-08-26 00:05:46 +0100

    feat: Add PWA support with manifest and basic SSL configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
   server: {
+    host: true,
     port: 3000,
-    host: '127.0.0.1', // Forces Vite to use the secure loopback IP
+    // host: '127.0.0.1', // Forces Vite to use the secure loopback IP
   }
 })
